8d4d0acf9ae1cbe9e8f6bb7baaf7bb2906400475
Problem 18 Dev <problemeighteen@gmail.com>
2026-01-22 01:14:41 +0100

    feat: biker, truck & player sprites + animations

--- scenes/obstacle/obstacle.gd
+++ scenes/obstacle/obstacle.gd
@@ -3,11 +3,13 @@ extends Area2D
 
 
 @export var speed := 150.0
-@export var direction := Vector2.LEFT
+@export var go_right := false
 
 
 func _process(delta: float) -> void:
+	var direction := Vector2.RIGHT if go_right else Vector2.LEFT
 	position += direction * speed * delta
+	$AnimatedSprite2D.flip_h = true if go_right else false
 
 
 func _on_body_entered(body: Node2D) -> void:
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -28,16 +28,29 @@ func _handle_movement() -> void:
 
 	if Input.is_action_just_pressed("move_up"):
 		_direction = Vector2.UP
+		$AnimatedSprite2D.play("walk_vertical")
 	elif Input.is_action_just_pressed("move_down"):
 		_direction = Vector2.DOWN
+		$AnimatedSprite2D.flip_h = true
+		$AnimatedSprite2D.play("walk_vertical")
 	elif Input.is_action_just_pressed("move_left"):
 		_direction = Vector2.LEFT
+		$AnimatedSprite2D.play("walk_side")
+		$AnimatedSprite2D.flip_h = true
 	elif Input.is_action_just_pressed("move_right"):
 		_direction = Vector2.RIGHT
+		$AnimatedSprite2D.play("walk_side")
+		$AnimatedSprite2D.flip_h = false
 
 	if _direction != Vector2.ZERO:
 		$MovementTimer.start()
 
 
 func _on_movement_timer_timeout() -> void:
+	match _direction:
+		Vector2.UP, Vector2.DOWN:
+			$AnimatedSprite2D.play("idle")
+		Vector2.LEFT, Vector2.RIGHT:
+			$AnimatedSprite2D.play("idle_side")
+
 	_direction = Vector2.ZERO
