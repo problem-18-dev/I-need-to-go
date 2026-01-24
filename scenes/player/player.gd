8579595bea90e79dde26c264e093ae898af9d20e
Problem 18 Dev <problemeighteen@gmail.com>
2026-01-24 21:42:01 +0100

    feat: refined obstacles, urinal

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
 
+signal player_died
+
 @export_category("Movement")
 @export var move_speed := 120.0
 @export var move_distance := 32.0
+
+var _can_be_controlled := true
 
 @onready var next_position := position
 
@@ -18,15 +22,12 @@ func _physics_process(delta: float) -> void:
 		$AnimatedSprite2D.play("idle")
 		direction = Vector2.ZERO
 
-	if velocity.is_zero_approx() and (position.x < 0 or position.x > get_viewport_rect().size.x):
-		die()
-
 	velocity = direction * move_speed
 	move_and_slide()
 
 
 func _unhandled_key_input(event: InputEvent) -> void:
-	if not velocity.is_zero_approx():
+	if not velocity.is_zero_approx() or not _can_be_controlled:
 		return
 
 	if event.is_action_pressed("move_up"):
@@ -47,7 +48,8 @@ func _unhandled_key_input(event: InputEvent) -> void:
 
 
 func die() -> void:
-	print("OH NO, I DIED")
+	$AnimatedSprite2D.play("death")
+	player_died.emit()
 
 
 func move_back() -> void:
@@ -55,3 +57,22 @@ func move_back() -> void:
 		next_position.y += move_distance
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("walk_vertical")
+
+
+func move_to(new_position: Vector2, cb = null) -> void:
+	if velocity.is_zero_approx():
+		next_position = new_position
+		$AnimatedSprite2D.play("walk_vertical")
+		if cb:
+			cb.call()
+
+
+func pee() -> void:
+	_can_be_controlled = false
+	$AnimatedSprite2D.play("pee")
+	await $AnimatedSprite2D.animation_finished
+	$AnimatedSprite2D.play("pee_loop")
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	die()
--- scenes/truck/truck.gd
+++ scenes/truck/truck.gd
@@ -1,6 +1,1 @@
 extends Obstacle
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	$AnimatedSprite2D.play("default")
